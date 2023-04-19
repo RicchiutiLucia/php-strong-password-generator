027fdb5ab25ea72019087d47a3f9ca67ba2a68b4
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-04-19 16:24:42 +0200

    aggunto un po di stile

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>Password Generator</title>
 </head>
 <body>
@@ -35,11 +37,21 @@
             }
         ?>
     </div>
-    <form action="index.php" method="GET">
+    <div class="mx-auto my-5 w-50 text-center">
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+    </div>
+
+    <form action="index.php" method="GET"  class="mx-auto my-3 w-50 text-center">
         <label for="password">Lunghezza Password:</label>
         <input type="number" id="password" name="password">
-        <button>Crea Password</button>
+        <div><button class="btn btn-primary my-5">Genera Password</button></div>
     </form>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     
 </body>
 </html>
