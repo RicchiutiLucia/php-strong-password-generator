805fb27bc8b1f6813e20cf6e913678ffffec7c76
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-04-20 11:01:47 +0200

    esercizio concluso

--- index.php
+++ index.php
@@ -51,8 +51,8 @@
         <h3 id="title">Strong password generator</h3>
         <div id="control">
             <?php
-                $lunghezzaPassword="";
-                if($lunghezzaPassword<=8 && $lunghezzaPassword>0){
+                $lunghezzaPassword ="";
+                if($lunghezzaPassword<=8 && $lunghezzaPassword>0 ){
                     ?><h3 id="alert">La password deve essere maggiore di 8!</h3><?php
                 }
             ?>
